088c7f6d71ed2f617b69812534ebe7d7f0b3cd28
centforever <centforever@163.com>
2024-01-20 19:42:28 +0800

    1

--- base_variant/build.gradle.kts
+++ base_variant/build.gradle.kts
@@ -53,7 +53,7 @@ android {
 }
 
 dependencies {
-    implementation(project(":flatbuffers"))
+    api(project(":flatbuffers"))
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
