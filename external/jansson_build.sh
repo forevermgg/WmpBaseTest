61c7274a6c032bc2c2f142b84f5c7a1b71f2ee27
centforever <centforever@163.com>
2024-01-21 00:13:01 +0800

    first commit

--- external/jansson_build.sh
+++ external/jansson_build.sh
@@ -24,3 +24,64 @@ if [ -d $PREFIX ]; then
 fi
 
 mkdir -p $PREFIX
+
+cd $WORKDIR
+if [ ! -d jansson ]; then
+    git clone --depth 10 https://github.com/akheron/jansson.git --branch v2.14
+fi
+
+cd jansson
+
+echo "Building jansson in $(realpath $PWD), deploying to $PREFIX"
+
+function build_jansson {
+  cd $WORKDIR/jansson
+  ANDROID_TARGET_ABI=$1
+
+  if [ "$ANDROID_TARGET_ABI" == "armeabi-v7a" ]
+  then
+    export TARGET=armv7a-linux-androideabi
+
+  elif [ "$ANDROID_TARGET_ABI" == "arm64-v8a" ]
+  then
+    export TARGET=aarch64-linux-android
+
+  elif [ "$ANDROID_TARGET_ABI" == "x86" ]
+  then
+    export TARGET=i686-linux-android
+
+  elif [ "$ANDROID_TARGET_ABI" == "x86_64" ]
+  then
+    export TARGET=x86_64-linux-android
+
+  else
+    echo "Unsupported target ABI: $ANDROID_TARGET_ABI"
+    exit 1
+  fi
+
+  echo "TARGET:${TARGET}"
+
+  mkdir -p build
+  cd build
+  cmake -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake \
+        -DANDROID_ABI=$1 \
+        -DANDROID_PLATFORM=${ANDROID_PLATFORM} \
+        -DCMAKE_BUILD_TYPE=Debug \
+        -DJANSSON_BUILD_SHARED_LIBS=ON \
+        -DCMAKE_INSTALL_PREFIX=$WORKDIR/prefix/jansson/${ANDROID_TARGET_ABI} \
+        -DANDROID_STL="c++_static" \
+        -DCMAKE_SHARED_LINKER_FLAGS="-lm" \
+        ../.
+  make && make install
+  echo "Build OK jansson, PWD in $(realpath $PWD)"
+  cd ../
+  # 必须得移除一下build，不然会编译失败
+  rm -rf build
+  cd ../
+}
+
+build_jansson armeabi-v7a
+build_jansson arm64-v8a
+build_jansson x86
+build_jansson x86_64
+ rm -rf jansson
