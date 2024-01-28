86e76d980368f843c76d49da3f175ecaf0d4d07d
centforever <centforever@163.com>
2024-01-28 20:52:27 +0800

    network

--- base_jni/src/main/cpp/base.cpp
+++ base_jni/src/main/cpp/base.cpp
@@ -1,12 +1,8 @@
 #include <jni.h>
 #include <string>
-#include "network/network_sharing_platform.h"
-#include "network/android_network_sharing_platform.h"
-#include "network/context_impl.h"
-#include "network/network_monitor.h"
-#include <memory>
+#include "TestData.h"
 
-std::unique_ptr<FOREVER::Context> context_;
+std::unique_ptr<TestData> test_data_;
 
 extern "C"
 JNIEXPORT jstring JNICALL
@@ -18,15 +14,12 @@ Java_com_mgg_base_NativeBaseLib_getVersion(JNIEnv *env, jobject thiz) {
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_mgg_base_NativeBaseLib_register(JNIEnv *env, jobject thiz) {
-  auto sharing_platform = FOREVER::AndroidNetWorkSharingPlatform();
-  context_ = std::make_unique<FOREVER::ContextImpl>((sharing_platform));
-  std::function<void(FOREVER::NetworkMonitor::ConnectionType, bool)> call_back =
-          [](FOREVER::NetworkMonitor::ConnectionType connection_type, bool is_lan_connected) {
-  };
+  test_data_ = std::make_unique<TestData>();
+  test_data_->TestExec();
 }
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_mgg_base_NativeBaseLib_unregister(JNIEnv *env, jobject thiz) {
-  context_.reset();
+  test_data_.reset();
 }
