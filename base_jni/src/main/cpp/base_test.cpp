82491f618b9e3433d525e691767d16d30c4b6b7d
forevermgg <centforever@163.com>
2024-01-31 14:57:18 +0800

    test

--- base_jni/src/main/cpp/base_test.cpp
+++ base_jni/src/main/cpp/base_test.cpp
@@ -5,13 +5,6 @@
 std::unique_ptr<TestData> test_data_;
 
 extern "C"
-JNIEXPORT jstring JNICALL
-Java_com_mgg_base_NativeBaseLib_getVersion(JNIEnv *env, jobject thiz) {
-    std::string hello = "Base JNI from C++";
-    return env->NewStringUTF(hello.c_str());
-}
-
-extern "C"
 JNIEXPORT void JNICALL
 Java_com_mgg_base_NativeBaseLib_register(JNIEnv *env, jobject thiz) {
   test_data_ = std::make_unique<TestData>();
